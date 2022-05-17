af2c1c2ef94e22a132b2461364edf770ab11d94f
Alex-Berezov <aleber197@gmail.com>
2022-05-17 22:35:20 +0400

    First try tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('With React Testing Library', () => {
+  const initialState = { output: 10 }
+  const mockStore = configureStore()
+  let store, wrapper
+
+  it('renders App', () => {
+    store = mockStore(initialState)
+    const {getByText} = render(<Provider store={store}><App /></Provider>)
+    screen.debug()
+    // expect(getByText('Hello Worldd!')).not.toBeNull()
+  })
+})
+// test('renders App', () => {
+  
+//   render(<App />)
+//   screen.debug()
+//   const linkElement = screen.getByText(/Доходы/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
---
+++ src/utils/index.test.ts
@@ -0,0 +1,5 @@
+import { getFinFormat } from './index'
+
+it('test for getFinFormat function', () => {
+  // expect(getFinFormat(1000)).toBe("1 000")
+})
